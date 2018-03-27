383686962cc140f4bebe708836ec861a9c21c550
Max Fowler <maxhfowler@gmail.com>
2018-03-27 14:46:27 -0700

    No css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,31 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div>
-    header
-  </div>
+    <div className="navbar overlaid">
+        <div className="myname">
+            <a href="/">MAX FOWLER</a>
+        </div>
+        <div className="navlinks">
+            <div className="navlink">
+                <a href="/about.html">about</a>
+            </div>
+            <div className="navlink">
+                <a href="/">work</a>
+            </div>
+            <div className="navlink">
+                <a href="http://twitter.com/notplants">twitter</a>
+            </div>
+            <div className="navlink">
+                <a href="http://github.com/mhfowler">github</a>
+            </div>
+            <div className="navlink">
+                <a href="/notes.html">notes</a>
+            </div>
+            <div className="navlink last-navlink">
+                <a href="/contact.html">contact</a>
+            </div>
+        </div>
+    </div>
 )
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
+import './index.scss'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="base-body">
     <Helmet
       title="Gatsby Default Starter"
       meta={[
@@ -15,14 +15,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
