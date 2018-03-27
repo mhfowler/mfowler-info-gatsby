f0e5639c92dbd46b3c7f4c5903904a1b0e05a101
Max Fowler <maxhfowler@gmail.com>
2018-03-26 17:49:51 -0700

    Basic setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = () => (
+  <div>
+    header
+  </div>
+)
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <p> test 100 </p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
