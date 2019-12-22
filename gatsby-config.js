0a6626cb319b120215ce837d345d34b06c94fa7f
Max Fowler <maxhfowler@gmail.com>
2019-12-22 13:43:05 -0500

    fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,25 +35,6 @@ module.exports = {
         name: `documentation`,
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-copy-linked-files`,
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 750,
-              backgroundColor: 'transparent',
-            },
-          },
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -3,28 +3,28 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   // all markdown remark
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
+  // const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  // const result = await graphql(`
+  //   {
+  //     allMarkdownRemark(
+  //       sort: { order: DESC, fields: [frontmatter___date] }
+  //       limit: 1000
+  //     ) {
+  //       edges {
+  //         node {
+  //           frontmatter {
+  //             path
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  // // Handle errors
+  // if (result.errors) {
+  //   reporter.panicOnBuild(`Error while running GraphQL query.`)
+  //   return
+  // }
   // result.data.allMarkdownRemark.edges.forEach(({ node }) => {
   //   if (node.frontmatter.path) {
   //     console.log(`++ creating md page ${node.frontmatter.path}`)
