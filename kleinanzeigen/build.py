27174f926b82438e6d37d101cc2c428c37ffcb48
Max Fowler <maxhfowler@gmail.com>
2020-05-12 22:38:33 +0200

    kleinanzeigen

---
+++ kleinanzeigen/build.py
@@ -0,0 +1,88 @@
+import os
+import jinja2
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+# input pages dir
+PAGES_DIR = os.path.join(PROJECT_PATH, 'kleinanzeigen/templates/pages')
+
+# output dirs
+LOCAL_OUTPUT_DIR = os.path.join(PROJECT_PATH, 'static/reuse/')
+PROD_OUTPUT_DIR = os.path.join(PROJECT_PATH, 'static/prod')
+if not os.path.exists(LOCAL_OUTPUT_DIR):
+    os.makedirs(LOCAL_OUTPUT_DIR)
+if not os.path.exists(PROD_OUTPUT_DIR):
+    os.makedirs(PROD_OUTPUT_DIR)
+
+# input inventory folder
+INVENTORY_DIR = os.path.join(PROJECT_PATH, 'static/kleinanzeigen')
+
+
+def load_inventory(inventory_dir):
+    inventory = {}
+    for dirpath, dirnames, filenames in os.walk(inventory_dir):
+        for f_name in filenames:
+            if f_name in ['.DS_Store']:
+                continue
+            img_path = '/kleinanzeigen/{}'.format(f_name)
+            inventory[f_name] = [img_path]
+        for dirname in dirnames:
+            if dirname == 'sold':
+                continue
+            full_path = os.path.join(dirpath, dirname)
+            img_paths = []
+            for f_name in os.listdir(full_path):
+                if f_name in ['.DS_Store']:
+                    continue
+                img_path = '/kleinanzeigen/{}/{}'.format(dirname, f_name)
+                img_paths.append(img_path)
+            inventory[dirname] = img_paths
+    return inventory
+
+
+
+def log_print(msg, debug):
+    if debug:
+        print(msg)
+
+
+def build_site(pages_dir, output_dir, debug):
+
+    # render html
+    template_path = os.path.join(PROJECT_PATH, 'kleinanzeigen/templates')
+    template_loader = jinja2.FileSystemLoader(searchpath=template_path)
+    template_env = jinja2.Environment( loader=template_loader, extensions=['jinja2.ext.with_'])
+
+    # load inventory
+    inventory = load_inventory(INVENTORY_DIR)
+    template_vars = {
+        'inventory': inventory
+    }
+
+    for dirpath, _, files in os.walk(pages_dir):
+        for page in files:
+            relative_dirpath = os.path.relpath(dirpath, pages_dir)
+            if relative_dirpath == '.':
+                relative_dirpath = ''
+            relative_path = os.path.join(relative_dirpath, page)
+            template_path = 'pages/' + relative_path
+            log_print('building: {}'.format(relative_path), debug=debug)
+            page_template = template_env.get_template(template_path)
+            page_text = page_template.render(template_vars)
+
+            output_page_path = os.path.join(output_dir, relative_path)
+            output_dirpath = os.path.dirname(output_page_path)
+            if not os.path.exists(output_dirpath):
+                os.makedirs(output_dirpath)
+            with open(output_page_path, 'w') as output_file:
+                page_text = str(page_text)
+                output_file.write(page_text)
+
+
+if __name__ == '__main__':
+
+    # build local dist
+    build_site(pages_dir=PAGES_DIR, output_dir=LOCAL_OUTPUT_DIR, debug=True)
+
+    # build prod site
+    # build_site(pages_dir=PAGES_DIR, output_dir=PROD_OUTPUT_DIR, template_vars=PROD_VARS, debug=True)
--- static/content/documentation/disconnection-practices/disconnection-practices.md
+++ static/content/documentation/disconnection-practices/disconnection-practices.md
@@ -12,8 +12,12 @@ Pin Daddy made its first appearance for The Disconnection Shop, a pop-up shop of
 
 Pin Daddy is a machine that can robotically child-block your phone with a pin that you don't know so that you can't access social media. After blocking your phone, the machine prints out your unknown pin encoded into a barcode on a receipt, which you can take with you and keep in a drawer or give to a friend. In the event that you need your pin again, you can ask any unblocked smart-phone owning person to scan your barcode using a barcode scanner app which decodes your pin. 
 
-As part of this pop-up shop, I also shared a zine of less automated Disconnection Practices (made in collaboration with Catherine Schmidt) which you can view as a PDF here: Disconnection Practices.pdf
+As part of this pop-up shop, I also shared a zine of less automated Disconnection Practices (made in collaboration with Catherine Schmidt) which you can view as a PDF here: 
+[disconnection-practices.pdf](https://arena-attachments.s3.amazonaws.com/3568310/9d8420005d1ff7ac32825f91553fa294.pdf?1549377955) 
+or order a physical copy here --> [physical zine](http://canalswans.net/zines/disconnection-practices)
 
 ![](disconnection-shop/caravan.jpg)
 ![](disconnection-shop/zines.jpeg)
 ![](disconnection-shop/the-disconnection-shop.png)
+
+<iframe src="https://player.vimeo.com/video/315455831" width="750" height="420" frameborder="0" allow="fullscreen" allowfullscreen></iframe>
